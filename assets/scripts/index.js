fb246ddf16ec1e1d8c815f5c6fefd8300e8648b7
luis gomez <luisgomezojeda110502@gmail.com>
2023-10-30 09:48:16 -0300

    add PWA

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,12 @@ import {getCurrentWheater} from "./getWheater.js";
 import {getCity,setCity} from "./localStorage.js";
 import {dinamic_bg} from "./dynamic_bg.js";
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+}
+
 document.addEventListener("DOMContentLoaded",(e)=>{
   getCity();
   const city = localStorage.getItem("city");
